e60b74628d9d3a13d9a7b97112f8dd2834c0d4c9
mast3r123 <abhishek.mast3r@gmail.com>
2022-01-06 16:18:01 -0500

    image picker controller and save image to disk

--- happydays/happydays/MemoriesViewController.swift
+++ happydays/happydays/MemoriesViewController.swift
@@ -13,11 +13,21 @@ import Speech
 class MemoriesViewController: UICollectionViewController {
     
     var memories = [URL]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         checkPermissions()
         loadMemories()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+    }
+    
+    @objc func addTapped() {
+        let vc = UIImagePickerController()
+        vc.modalPresentationStyle = .formSheet
+        vc.delegate = self
+        navigationController?.present(vc, animated: true, completion: nil)
     }
     
     func loadMemories() {
@@ -39,7 +49,30 @@ class MemoriesViewController: UICollectionViewController {
         }
         collectionView.reloadSections(IndexSet(integer: 1))
     }
-
+    
+    func saveNewMemory(image: UIImage) {
+        //create memory name
+        let memoryName = "memory-\(Date().timeIntervalSince1970)"
+        let imageName = memoryName + ".jpeg"
+        let thumbName = memoryName + ".thumb"
+        
+        do {
+            // create a URL where we can write the JPEG to
+            let imagePath =
+            getDocumentsDirectory().appendingPathComponent(imageName)
+            // convert the UIImage into a JPEG data object
+            if let jpegData = image.jpegData(compressionQuality: 0.8)
+            {
+                // write that data to the URL we created
+                try jpegData.write(to: imagePath, options:
+                                    [.atomicWrite])
+            }
+            // create thumbnail here
+        } catch {
+            print("Failed to save to disk.")
+        }
+    }
+    
     func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let documentsDirectory = paths[0]
@@ -57,6 +90,17 @@ class MemoriesViewController: UICollectionViewController {
             if let vc = storyboard?.instantiateViewController(withIdentifier: "FirstRun") {
                 navigationController?.present(vc, animated: true, completion: nil)
             }
+        }
+    }
+}
+
+extension MemoriesViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true, completion: nil)
+        
+        if let possibleImage = info[.originalImage] as? UIImage {
+            saveNewMemory(image: possibleImage)
+            loadMemories()
         }
     }
 }
