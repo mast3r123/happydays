a7995dc5ac97018fcb384ce92015024a690befa5
mast3r123 <abhishek.mast3r@gmail.com>
2022-01-06 13:36:15 -0500

    Added outlets for cell and controller

---
+++ happydays/happydays/MemoriesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesViewController.swift
+//  happydays
+//
+//  Created by master on 1/6/22.
+//
+
+import UIKit
+
+class MemoriesViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ happydays/happydays/MemoryCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemoryCell.swift
+//  happydays
+//
+//  Created by master on 1/6/22.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
