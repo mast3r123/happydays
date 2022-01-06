f0493afd09cd90a7297972ddb35149e4e4e25616
mast3r123 <abhishek.mast3r@gmail.com>
2022-01-06 13:48:59 -0500

    Permission checks

--- happydays/happydays/ViewController.swift
+++ happydays/happydays/ViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import Speech
 
 class ViewController: UIViewController {
 
@@ -16,7 +19,48 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func requestPhotoPermissions() {
+        PHPhotoLibrary.requestAuthorization(for: .readWrite) { [unowned self] authStatus  in
+            DispatchQueue.main.async {
+                if authStatus == .authorized {
+                    self.requestRecordPermissions()
+                } else {
+                    self.helpLabel.text = "Photos permission was declined; please enable it in settings and tap Continue again."
+                }
+            }
+        }
+    }
+    
+    func requestRecordPermissions() {
+        AVAudioSession.sharedInstance().requestRecordPermission { [unowned self] allowed in
+            DispatchQueue.main.async {
+                if allowed {
+                    self.requestTranscribePermissions()
+                } else {
+                    self.helpLabel.text = "Recording permission was declined; please enable it in settings then tap Continue again."
+                }
+            }
+        }
+    }
+    
+    func requestTranscribePermissions() {
+        SFSpeechRecognizer.requestAuthorization { [unowned self] authStatus in
+            DispatchQueue.main.async {
+                if authStatus == .authorized {
+                    self.authorizationComplete()
+                } else {
+                    self.helpLabel.text = "Transcription permission was declined; please enable it in settings then tap Continue again."
+                }
+            }
+        }
+    }
+    
+    func authorizationComplete() {
+        dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func requestPermissions(_ sender: UIButton) {
+        requestPhotoPermissions()
     }
     
 }
