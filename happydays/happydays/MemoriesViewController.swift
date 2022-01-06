56092468e16df4d5c01bc57596ca31309c739cd9
mast3r123 <abhishek.mast3r@gmail.com>
2022-01-06 16:02:06 -0500

    load users memories and append to memory object

--- happydays/happydays/MemoriesViewController.swift
+++ happydays/happydays/MemoriesViewController.swift
@@ -11,12 +11,41 @@ import Photos
 import Speech
 
 class MemoriesViewController: UICollectionViewController {
+    
+    var memories = [URL]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         checkPermissions()
+        loadMemories()
+    }
+    
+    func loadMemories() {
+        memories.removeAll()
+        
+        //load all memories in document directory
+        guard let files = try? FileManager.default.contentsOfDirectory(at: getDocumentsDirectory(), includingPropertiesForKeys: nil, options: []) else { return }
+        
+        //loop over everyfile found
+        for file in files {
+            let fileName = file.lastPathComponent
+            
+            if fileName.hasSuffix(".thumb") {
+                //get room name of memory
+                let noExtension = fileName.replacingOccurrences(of: ".thumb", with: "")
+                let memoryPath = getDocumentsDirectory().appendingPathComponent(noExtension)
+                memories.append(memoryPath)
+            }
+        }
+        collectionView.reloadSections(IndexSet(integer: 1))
     }
 
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
     func checkPermissions() {
         let photoAuthorized = PHPhotoLibrary.authorizationStatus() == .authorized
         let recordingAuthorized = AVAudioSession.sharedInstance().recordPermission == .granted
