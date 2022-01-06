25a423c9b3010536fa4faa70c1418fd76948cb10
mast3r123 <abhishek.mast3r@gmail.com>
2022-01-06 15:50:05 -0500

    Check permission in memory controller, change initial view controller

--- happydays/happydays/MemoriesViewController.swift
+++ happydays/happydays/MemoriesViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import Speech
 
 class MemoriesViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        checkPermissions()
     }
 
+    func checkPermissions() {
+        let photoAuthorized = PHPhotoLibrary.authorizationStatus() == .authorized
+        let recordingAuthorized = AVAudioSession.sharedInstance().recordPermission == .granted
+        let transcribeAuthorized = SFSpeechRecognizer.authorizationStatus() == .authorized
+        
+        let authorized = photoAuthorized && recordingAuthorized && transcribeAuthorized
+        
+        if !authorized {
+            if let vc = storyboard?.instantiateViewController(withIdentifier: "FirstRun") {
+                navigationController?.present(vc, animated: true, completion: nil)
+            }
+        }
+    }
 }
